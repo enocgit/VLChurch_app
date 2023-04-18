3419b7fdf2236028e7c3a00cad5100a689d9432b
Enoch Ansah <enocansah@yahoo.com>
2023-04-18 23:05:44 +0000

    Modify: Environment variables settings

--- members/models.py
+++ members/models.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 # members
-class Member(AbstractUser): # members inheriting from AbstractUser model
+class Member(AbstractUser):  # members inheriting from AbstractUser model
     # username = models.CharField(max_length=100, unique=True)
     # name = models.CharField(max_length=255, null=True, blank=True)
-    picture = models.ImageField(upload_to='static/uploads', height_field=None, width_field=None, max_length=None, null=True, blank=True, default='static/images/no_image.jpeg')
+    picture = models.ImageField(upload_to='static/uploads', height_field=None, width_field=None,
+                                max_length=None, null=True, blank=True, default='static/images/no_image.jpeg')
     birthday = models.DateField(null=True, blank=True)
     # fellowship = models.ForeignKey(Fellowship, null=True, blank=True, on_delete=models.SET_NULL)
-    fellowship = models.ManyToManyField('fellowships.Fellowship', verbose_name='fellowship group', null=True, blank=True)
+    fellowship = models.ManyToManyField(
+        'fellowships.Fellowship', verbose_name='fellowship group', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        # return f'{self.first_name} {self.last_name}'
+        return self.username
--- vlchurch_app/settings.py
+++ vlchurch_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-29no%^g9flp8gx(_e=8&+@+p$_k3wrelhzk8u#=c7o=$rk#2+q'
+# SECRET_KEY = 'django-insecure-29no%^g9flp8gx(_e=8&+@+p$_k3wrelhzk8u#=c7o=$rk#2+q'
+dotenv.load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -95,7 +99,8 @@ WSGI_APPLICATION = 'vlchurch_app.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 
