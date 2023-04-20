7bd9692a7a0cffc2db8055d2d336d6d370779d50
Enoch Ansah <enocansah@yahoo.com>
2023-04-20 18:33:20 +0000

    Add: Payment modal

--- members/models.py
+++ members/models.py
@@ -8,7 +8,7 @@ class Member(AbstractUser):  # members inheriting from AbstractUser model
     # username = models.CharField(max_length=100, unique=True)
     # name = models.CharField(max_length=255, null=True, blank=True)
     picture = models.ImageField(upload_to='images', height_field=None, width_field=None,
-                                max_length=None, null=True, blank=True, default='/static/images/no-image.png')
+                                max_length=None, null=True, blank=True, )
     # picture = DefaultStaticImageField(upload_to='images', height_field=None, width_field=None, max_length=None, null=True, blank=True, default_image_path='/static/images/no-image.png')
     birthday = models.DateField(null=True, blank=True)
     # fellowship = models.ForeignKey(Fellowship, null=True, blank=True, on_delete=models.SET_NULL)
